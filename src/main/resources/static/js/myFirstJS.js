bc8d799fda7c8d103d1d9f04449e0b1e11e0636a
amorro <ams00@iesemilidarder.com>
2019-02-20 20:56:18 +0100

    20 feb

--- src/main/resources/static/js/myFirstJS.js
+++ src/main/resources/static/js/myFirstJS.js
@@ -1,12 +1,35 @@
 var commonFunctions = {
+    myData: ("Hola","don", "Pipo"),
     helloWorld: function () {
         alert("VOLAD!");
     },
-    askMeSomething:function () {
-        if (confirm("Estas seguro NEN??")){
+    askMeSomething: function () {
+        if (confirm("Estas seguro NEN??")) {
             alert("Toma truño");
-        }else{
+        } else {
             alert("ME ABURROOOOO");
         }
-    }
+    },
+    askMeBiconditions: function () {
+        // a?  b (OK) OR c (Cancel)
+        confirm("estas seguro?") ? alert("yuhu") : alert("no yuhu");
+    },
+    showMessage: function () {
+        var msg = document.getElementById("messageAlert");
+        console.log(msg);
+        msg.style.display = "inline";
+    },
+    blockMe: function () {
+        for (i = 0; i < 10; i++) {
+            alert("Alert " + i);
+        }
+    },
+  alertInput:function(id) {
+        alert (document.getElementById(id).value);
+  }
 };
+
+//blockForEach: function(){
+//    this.myData.forEach(e = >
+//)
+
