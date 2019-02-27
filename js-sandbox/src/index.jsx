31ec73384d5f5acdae35de7225686a1af561aa5d
amorro <ams00@iesemilidarder.com>
2019-02-27 20:55:50 +0100

    babel

---
+++ js-sandbox/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title= "Hi cosa";
+
+ReactDOM.render(
+    <h1>{title}</h1>,
+    document.getElementById('app')
+);
+
+console.log("cosa");
